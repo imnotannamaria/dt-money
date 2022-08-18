344d84c30f0441909a586c7b96c5d1e153b46644
Anna Maria <aannamariabr@outlook.com>
2022-08-18 16:52:40 -0300

    Formatando datas e preços

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,6 +1,7 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { useContext } from 'react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { priceFormmatter } from '../../utils/formatter';
 import { SummaryCard, SummatyContainer } from './styles'
 
 export function Summary() {
@@ -34,7 +35,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormmatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -43,7 +44,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormmatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -52,7 +53,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormmatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummatyContainer>
   )
