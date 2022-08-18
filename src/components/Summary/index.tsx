73cf990403f22bd46f58bebca5237413063be2eb
Anna Maria <aannamariabr@outlook.com>
2022-08-18 16:42:49 -0300

    Adicionando lógica no componente de resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,7 +5,27 @@ import { SummaryCard, SummatyContainer } from './styles'
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
-  
+
+  const summary = transactions.reduce
+  (
+    (acc, transaction) => {
+      if(transaction.type === 'income') {
+        acc.income = acc.income + transaction.price;
+        acc.total = acc.total + transaction.price;
+      } else {
+        acc.outcome = acc.outcome + transaction.price;
+        acc.total = acc.total - transaction.price;
+      }
+
+      return acc;
+    }, 
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
   return (
     <SummatyContainer>
       <SummaryCard>
@@ -14,7 +34,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -23,7 +43,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
 
-        <strong>R$ 1.259,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -32,7 +52,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 16.141,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummatyContainer>
   )
